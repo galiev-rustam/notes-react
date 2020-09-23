c4cdc77746c8a82443a7dee9208ce6e23f12b3b8
Rustam <galiev-rustam9@users.noreply.github.com>
2020-09-24 00:18:14 +0500

    first commit

---
+++ src/note.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="note">
+        <div className="header"></div>
+      </div>
+    );
+  }
+}
+
+export default App;
